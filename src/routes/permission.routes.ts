6091c493d2015d5b64aad7d854e0fa374afed3c5
Carlos61155 <carloshenrique021deus13@gmail.com>
2026-06-03 14:19:46 -0300

    service/controller Permission

---
+++ src/routes/permission.routes.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { PermissionController } from "@/controller/PermissionController";
+
+const router = Router();
+
+const controller = new PermissionController();
+
+
