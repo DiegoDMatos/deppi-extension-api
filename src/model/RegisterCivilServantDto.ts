bfb1ca170afee2c56348834a9aec71fe249c1fe6
Carlos61155 <carloshenrique021deus13@gmail.com>
2026-07-01 07:29:39 -0300

    feat/atualização migrations

--- src/model/RegisterCivilServantDto.ts
+++ src/model/RegisterCivilServantDto.ts
@@ -3,6 +3,33 @@ export interface RegisterCivilServantDto {
     email: string;
     password: string;
     
+    personalPhones: string[];
+    cpf: string;
+    registrationName: string;
+    dateOfBirth: Date;
+    maritalStatus: string;
+    placeOfBirth: string;   
+    sex: string;
+    bloodType?: string;
+    rhFactor?: string;   
+    numberOfDependents: number;   
+    raceEthnicity: string;
+    fatherName?: string;  
+    motherName?: string;
+    pisPasep: string;
+    academicTitle: string;
+    educationLevel: string;   
+    address: string;
+    identityNumber: string;
+    issuingAgency: string;
+    issuingState: string;
+    issueDate: Date;
+    voterRegistrationNumber: string;
+    electoralZone: string;
+    electoralSection: string;
+    voterRegistrationState: string;  
+
+
     registration?: string;
     preferredName?: string;
     institutionalEmail: string;
